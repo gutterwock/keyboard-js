4c7ba13bc0d9d308742d7a723f0b12259f2625ca
tony feng <thefunkyphresh@gmail.com>
2024-05-03 19:30:34 -0400

    initial commit

---
+++ src/macropad/config/index.js
@@ -0,0 +1,7 @@
+const columns = 6;
+const rows = 3;
+
+module.exports = {
+  columns,
+  rows
+};
---
+++ src/switches/gateron.js
@@ -0,0 +1,9 @@
+const baseCellWidth = 19;
+const plateThickness = 1.2;
+const slotWidth = 14;
+
+module.exports = {
+  baseCellWidth,
+  plateThickness,
+  slotWidth
+};
